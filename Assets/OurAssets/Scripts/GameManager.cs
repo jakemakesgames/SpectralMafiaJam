70934ef957491dc90edb93fa6524ae46f9beed03
Jeremy Little <jezzalittle20@gmail.com>
2018-05-21 15:47:58 +1000

    GameManager

---
+++ Assets/OurAssets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
+    private GameObject player;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+
+
+}
