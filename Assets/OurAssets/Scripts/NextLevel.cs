611dd759c9edb54242d9ba785a402e0c8301d2f2
Jeremy Little <jezzalittle20@gmail.com>
2018-05-22 11:13:13 +1000

    setting up change levels

---
+++ Assets/OurAssets/Scripts/NextLevel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    GameManager gm;
+    bool player1In;
+    bool player2In;
+
+    void Start()
+    {
+        gm = GameManager.Instance;
+    }
+
+    void Update()
+    {
+        if (player1In == true && player2In == true)
+        {
+            gm.ChangeLevel();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == gm.Player1GO)
+        {
+            if (gm.Player1GO != null)
+            {
+                if (gm.Player2GO == null)
+                {
+                    gm.ChangeLevel();
+                }
+                else
+                {
+                    player1In = true;
+                }
+            }
+
+        }
+        if (other.gameObject == gm.Player2GO)
+        {
+            player2In = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == gm.Player1GO)
+        {
+            player1In = false;
+        }
+        if (other.gameObject == gm.Player2GO)
+        {
+            player2In = false;
+        }
+
+
+    }
+}
