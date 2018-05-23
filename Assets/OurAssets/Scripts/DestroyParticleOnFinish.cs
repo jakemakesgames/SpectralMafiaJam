29d9c4ff000874255972c65362dd608dd2690eca
Gene Collis <genecollis12@gmail.com>
2018-05-23 10:17:14 +1000

    Changed vortex particle prefab, added a destroy particle on finish script

---
+++ Assets/OurAssets/Scripts/DestroyParticleOnFinish.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleOnFinish : MonoBehaviour
+{
+    void Awake ()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        Destroy(gameObject, ps.main.duration);
+	}
+}
